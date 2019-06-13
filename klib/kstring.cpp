e5ae1b9298202089b37ae7492b70e1807908d4eb
IGR2014 <igooor7@gmail.com>
2019-06-14 02:29:47 +0300

    (klib) Fixed kstrlen and kstrcpy

--- klib/kstring.cpp
+++ klib/kstring.cpp
@@ -3,13 +3,15 @@
 //	Kernel string functions
 //
 //	File:	string.cpp
-//	Date:	06 Jun 2019
+//	Date:	14 Jun 2019
 //
 //	Copyright (c) 2017 - 2019, Igor Baklykov
 //	All rights reserved.
 //
 //
 
+
+#include <cstdint>
 
 #include <klib/kstring.hpp>
 #include <klib/memset.hpp>
@@ -20,29 +22,42 @@ namespace klib {
 
 
 	// Calculate string length
-	dword_t kstrlen(const sbyte_t* src) {
+	std::size_t kstrlen(const sbyte_t* src) {
 
-                // String length value
-                dword_t strLength = 0;
-
-                // Calculate string length
-                while (*src++ != '\0') {};
-
-                // Return result
-                return strLength;
+		// Soure string's start pointer
+		auto srcPtr = src;
+		// Get source string end pointer
+		for (; *srcPtr != '\0'; ++srcPtr);
+		// Calculate string length
+		return (srcPtr - src);
 
         }
 
 	// Copy string from one to other
-	void kstrcpy(const sbyte_t* src, sbyte_t* dst, dword_t size) {
+	sbyte_t* kstrcpy(const sbyte_t* src, sbyte_t* dst, std::size_t size) {
 
-                // Copy string
-                while (--size > 0) {
+		// Destination pointer
+		auto dstPtr = dst;
 
-                        // Copy string byte by byte
-                        *dst++ = *src++;
+		// Check src, dst and size
+                if (src == nullptr || dst == nullptr || size == 0) {
 
-                }
+			return dstPtr;
+
+		}
+
+		// Copy first symbol from src to dst
+		*dst = *src;
+
+		// Copy string while possible
+		do {
+
+			*++dst = *++src;
+                    
+		} while (--size > 0 && *src != '\0');
+
+		// Destination address
+		return dstPtr;
 
         }
 
