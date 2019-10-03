2b5373211cfa11b808b08cd2f2321b174be77a5b
IGR2014 <igooor7@gmail.com>
2019-10-03 17:59:39 +0300

    Small refactoring

--- arch/i386/paging.cpp
+++ arch/i386/paging.cpp
@@ -3,7 +3,7 @@
 //	Memory paging for x86
 //
 //	File:	paging.cpp
-//	Date:	18 Jun 2019
+//	Date:	02 Oct 2019
 //
 //	Copyright (c) 2017 - 2019, Igor Baklykov
 //	All rights reserved.
@@ -51,30 +51,23 @@ namespace arch {
 
 		// Other page directories are unused
 		for (dword_t i = 0; i < 1024; ++i) {
-
 			// Pages marked as clear
 			pageDirectory[i] = dword_t(pagingFlags_t::CLEAR);
-
 		}
-
 		// Map first 4 MB of physical RAM to first 4 MB of virtual RAM
 		pageDirectory[0]	 = dword_t(pageTable) & 0x3FFFFFFF;
 		pageDirectory[0]	|= dword_t(pagingFlags_t::WRITABLE | pagingFlags_t::PRESENT);
 
 		// Map all pages of first 4MB to first page table
 		for (sdword_t j = 0; j < 1024; ++j) {
-
 			pageTable[j]	 = (j << 12);
 			pageTable[j]	|= dword_t(pagingFlags_t::WRITABLE | pagingFlags_t::PRESENT);
-
 		}
-
 		// Also map first 4MB of physical RAM to first 4MB after 3GB in virtual memory
 		// (This should be useful for higher-half kernel)
 		pageDirectory[768]	 = dword_t(pageTable) & 0x3FFFFFFF;
 		// Page marked as present and writable
 		pageDirectory[768]	|= dword_t(pagingFlags_t::WRITABLE | pagingFlags_t::PRESENT);
-
 		// Map page table itself to the last page of virtual memory
 		pageDirectory[1023]	 = dword_t(pageDirectory) & 0x3FFFFFFF;
 		// Page marked as present and writable
@@ -94,39 +87,31 @@ namespace arch {
 	pointer_t pagingVirtToPhys(const pointer_t virtAddr) {
 
 		// Page directory entry index from virtual address
-		dword_t pdEntryIndex	= (dword_t(virtAddr) & 0xFFC00000) >> 22;
+		dword_t	pdEntryIndex	= (dword_t(virtAddr) & 0xFFC00000) >> 22;
 		// Page table entry index from virtual address
-		dword_t ptEntryIndex	= (dword_t(virtAddr) & 0x3FF000) >> 12;
-
+		dword_t	ptEntryIndex	= (dword_t(virtAddr) & 0x3FF000) >> 12;
 		// Physical pointer to page table
 		dword_t	pageTablePtr	= pageDirectory[pdEntryIndex];
 		auto	pageFlags	= pagingFlags_t(pageTablePtr & 0xFFF);
-
 		// Check if page table is present or not
 		if ((pageFlags & pagingFlags_t::PRESENT) == pagingFlags_t::CLEAR) {
-
 			// Page or table is not present
 			return nullptr;
-
 		}
 
 		// Physical pointer to page
 		dword_t pagePtr	= reinterpret_cast<dword_t*>(pageTablePtr & ~0xFFF)[ptEntryIndex];
 		pageFlags	= pagingFlags_t(pagePtr & 0xFFF);
-
 		// Check if page is present or not
 		if ((pageFlags & pagingFlags_t::PRESENT) == pagingFlags_t::CLEAR) {
-
 			// Page or table is not present
 			return nullptr;
-
 		}
 
 		// Get physical address of page from page table (20 MSB)
 		dword_t physPageAddr	= pagePtr & ~0xFFF;
 		// Get physical offset from virtual address`s (12 LSB)
 		dword_t physPageOffset	= dword_t(virtAddr) & 0xFFF;
-
 		// Return physical address
 		return pointer_t(physPageAddr | physPageOffset);
 
