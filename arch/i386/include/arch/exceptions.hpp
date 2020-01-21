51986a11069b06ce405f837f5395ac46e529f4da
IGR2014 <igooor7@gmail.com>
2020-01-21 23:16:38 +0200

    (exceptions) Exceptions refactored to be more modern-like

--- arch/i386/include/arch/exceptions.hpp
+++ arch/i386/include/arch/exceptions.hpp
@@ -3,7 +3,7 @@
 //	Exceptions low-level operations
 //
 //	File:	exceptions.hpp
-//	Date:	20 Jan 2020
+//	Date:	21 Jan 2020
 //
 //	Copyright (c) 2017 - 2020, Igor Baklykov
 //	All rights reserved.
@@ -22,67 +22,6 @@
 
 // Arch-dependent code zone
 namespace arch {
-
-
-	// Exceptions names
-	constexpr const sbyte_t* exName[32] {
-		u8"DIVIDE BY ZERO",			// 0
-		u8"DEBUG",				// 1
-		u8"NON-MASKABLE INTERRUPT",		// 2
-		u8"BREAKPOINT",				// 3
-		u8"INTO DETECTED OVERFLOW",		// 4
-		u8"BOUND RANGE EXCEEDED",		// 5
-		u8"INVALID OPCODE",			// 6
-		u8"NO COPROCESSOR",			// 7
-		u8"DOUBLE FAULT",			// 8
-		u8"COPROCESSOR SEGMENT OVERRUN",	// 9
-		u8"INVALID TSS",			// 10
-		u8"SEGMENT NOT PPRESENT",		// 11
-		u8"STACK FAULT",			// 12
-		u8"GENERAL PROTECTION FAULT",		// 13
-		u8"PAGE FAULT",				// 14
-		u8"UNKNOWN INTERRUPT",			// 15
-		u8"COPROCESSOR FAULT",			// 16
-		u8"ALIGNMENT CHECK",			// 17
-		u8"MACHINE CHECK",			// 18
-		u8"RESERVED",				// 19
-		u8"RESERVED",				// 20
-		u8"RESERVED",				// 21
-		u8"RESERVED",				// 22
-		u8"RESERVED",				// 23
-		u8"RESERVED",				// 24
-		u8"RESERVED",				// 25
-		u8"RESERVED",				// 26
-		u8"RESERVED",				// 27
-		u8"RESERVED",				// 28
-		u8"RESERVED",				// 29
-		u8"RESERVED",				// 30
-		u8"RESERVED"				// 31
-	};
-
-
-	// Interrupts number enumeration
-	enum exNumber_t : dword_t {
-		DIVIDE_BY_ZERO			= 0,
-		DEBUG				= 1,
-		NON_MASKABLE_IRQ		= 2,
-		BREAKPOINT			= 3,
-		INTO_DETECTED_OVERFLOW		= 4,
-		BOUND_RANGE_EXCEEDED		= 5,
-		INVALID_OPCODE			= 6,
-		NO_COPROCESSOR			= 7,
-		DOUBLE_FAULT			= 8,
-		COPROCESSOR_SEGMENT_OVERRUN	= 9,
-		INVALID_TSS			= 10,
-		SEGMENT_NOT_PRESENT		= 11,
-		STACK_FAULT			= 12,
-		GENERAL_PROTECTION_FAULT	= 13,
-		PAGE_FAULT			= 14,
-		UNKNOWN_IRQ			= 15,
-		COPROCESSOR_FAULT		= 16,
-		ALIGNMENT_CHECK			= 17,
-		MACHINE_CHECK			= 18
-	};
 
 
 #ifdef	__cplusplus
@@ -165,16 +104,113 @@ namespace arch {
 #endif	// __cplusplus
 
 
+	// Task registers structure forward declaration
+	struct taskRegs_t;
+
+
+	// Exceptions structure
+	class except final {
+
+	public:
+
+		// Exceptions names
+		constexpr static const sbyte_t* const NAME[32] {
+			u8"DIVIDE BY ZERO",			// 0
+			u8"DEBUG",				// 1
+			u8"NON-MASKABLE INTERRUPT",		// 2
+			u8"BREAKPOINT",				// 3
+			u8"INTO DETECTED OVERFLOW",		// 4
+			u8"BOUND RANGE EXCEEDED",		// 5
+			u8"INVALID OPCODE",			// 6
+			u8"NO COPROCESSOR",			// 7
+			u8"DOUBLE FAULT",			// 8
+			u8"COPROCESSOR SEGMENT OVERRUN",	// 9
+			u8"INVALID TSS",			// 10
+			u8"SEGMENT NOT PPRESENT",		// 11
+			u8"STACK FAULT",			// 12
+			u8"GENERAL PROTECTION FAULT",		// 13
+			u8"PAGE FAULT",				// 14
+			u8"UNKNOWN INTERRUPT",			// 15
+			u8"COPROCESSOR FAULT",			// 16
+			u8"ALIGNMENT CHECK",			// 17
+			u8"MACHINE CHECK",			// 18
+			u8"RESERVED",				// 19
+			u8"RESERVED",				// 20
+			u8"RESERVED",				// 21
+			u8"RESERVED",				// 22
+			u8"RESERVED",				// 23
+			u8"RESERVED",				// 24
+			u8"RESERVED",				// 25
+			u8"RESERVED",				// 26
+			u8"RESERVED",				// 27
+			u8"RESERVED",				// 28
+			u8"RESERVED",				// 29
+			u8"RESERVED",				// 30
+			u8"RESERVED"				// 31
+		};
+
+		// Exceptions number enumeration
+		enum class NUMBER : dword_t {
+			DIVIDE_BY_ZERO			= 0,
+			DEBUG				= 1,
+			NON_MASKABLE_IRQ		= 2,
+			BREAKPOINT			= 3,
+			INTO_DETECTED_OVERFLOW		= 4,
+			BOUND_RANGE_EXCEEDED		= 5,
+			INVALID_OPCODE			= 6,
+			NO_COPROCESSOR			= 7,
+			DOUBLE_FAULT			= 8,
+			COPROCESSOR_SEGMENT_OVERRUN	= 9,
+			INVALID_TSS			= 10,
+			SEGMENT_NOT_PRESENT		= 11,
+			STACK_FAULT			= 12,
+			GENERAL_PROTECTION_FAULT	= 13,
+			PAGE_FAULT			= 14,
+			UNKNOWN_IRQ			= 15,
+			COPROCESSOR_FAULT		= 16,
+			ALIGNMENT_CHECK			= 17,
+			MACHINE_CHECK			= 18
+		};
+
+
+		// Default c-tor
+		except() noexcept = default;
+
+		// Copy c-tor
+		except(const except &other) = delete;
+		// Copy assignment
+		except& operator=(const except &other) = delete;
+
+		// Move c-tor
+		except(except &&other) = delete;
+		// Move assignment
+		except& operator=(except &&other) = delete;
+
+		// Init exceptions
+		static void init() noexcept;
+
+		// Install exceptions handler
+		constexpr static void install(const NUMBER irqNumber, const isrHandler_t irqHandler) noexcept;
+		// Uninstall exceptions handler
+		constexpr static void uninstall(const NUMBER irqNumber) noexcept;
+
+		// Default exception handler
+		static void exDefaultHandler(const taskRegs_t* regs) noexcept;
+
+
+	};
+
+
 	// Install handler
-	constexpr void exHandlerInstall(const exNumber_t exNumber, const isrHandler_t handler) {
+	constexpr void except::install(const NUMBER exNumber, const isrHandler_t handler) noexcept {
 		// Install ISR
-		isrHandlerInstall(dword_t(exNumber), handler);
+		isrHandlerInstall(static_cast<dword_t>(exNumber), handler);
 	}
 
 	// Uninstall handler
-	constexpr void exHandlerUninstall(const exNumber_t exNumber) {
+	constexpr void except::uninstall(const NUMBER exNumber) noexcept {
 		// Uninstall ISR
-		isrHandlerUninstall(dword_t(exNumber));
+		isrHandlerUninstall(static_cast<dword_t>(exNumber));
 	}
 
 
--- arch/x86_64/exceptions.cpp
+++ arch/x86_64/exceptions.cpp
@@ -3,7 +3,7 @@
 //	Exceptions low-level operations
 //
 //	File:	exceptions.cpp
-//	Date:	20 Jan 2020
+//	Date:	21 Jan 2020
 //
 //	Copyright (c) 2017 - 2020, Igor Baklykov
 //	All rights reserved.
@@ -12,13 +12,47 @@
 
 
 #include <arch/exceptions.hpp>
+#include <arch/taskRegs.hpp>
+
+#include <klib/kprint.hpp>
 
 
 // Arch-dependent code zone
 namespace arch {
 
 
-	//
+	// Init exceptions
+	void except::init() noexcept {
+		// Set default exception handlers
+		except::install(except::NUMBER::DIVIDE_BY_ZERO,			except::exDefaultHandler);
+		except::install(except::NUMBER::DEBUG,				except::exDefaultHandler);
+		except::install(except::NUMBER::NON_MASKABLE_IRQ,		except::exDefaultHandler);
+		except::install(except::NUMBER::BREAKPOINT,			except::exDefaultHandler);
+		except::install(except::NUMBER::INTO_DETECTED_OVERFLOW,		except::exDefaultHandler);
+		except::install(except::NUMBER::BOUND_RANGE_EXCEEDED,		except::exDefaultHandler);
+		except::install(except::NUMBER::INVALID_OPCODE,			except::exDefaultHandler);
+		except::install(except::NUMBER::NO_COPROCESSOR,			except::exDefaultHandler);
+		except::install(except::NUMBER::DOUBLE_FAULT,			except::exDefaultHandler);
+		except::install(except::NUMBER::COPROCESSOR_SEGMENT_OVERRUN,	except::exDefaultHandler);
+		except::install(except::NUMBER::INVALID_TSS,			except::exDefaultHandler);
+		except::install(except::NUMBER::SEGMENT_NOT_PRESENT,		except::exDefaultHandler);
+		except::install(except::NUMBER::STACK_FAULT,			except::exDefaultHandler);
+		except::install(except::NUMBER::GENERAL_PROTECTION_FAULT,	except::exDefaultHandler);
+		except::install(except::NUMBER::PAGE_FAULT,			except::exDefaultHandler);
+		except::install(except::NUMBER::UNKNOWN_IRQ,			except::exDefaultHandler);
+		except::install(except::NUMBER::COPROCESSOR_FAULT,		except::exDefaultHandler);
+		except::install(except::NUMBER::ALIGNMENT_CHECK,		except::exDefaultHandler);
+		except::install(except::NUMBER::MACHINE_CHECK,			except::exDefaultHandler);
+	}
+
+
+	// Default exception handler
+	void except::exDefaultHandler(const taskRegs_t* regs) noexcept {
+		// Print exception name
+		klib::kprintf("Exception:\t%s", except::NAME[regs->number]);
+		// Hang CPU
+		while(true) {};
+	}
 
 
 }	// namespace arch
