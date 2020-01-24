858e04af0acbb24736dcc1145137efca21049265
IGR2014 <igooor7@gmail.com>
2020-01-24 04:19:59 +0200

    (version) Compile-time macros

--- include/version.hpp
+++ include/version.hpp
@@ -3,7 +3,7 @@
 //	IgrOS version info
 //
 //	File:	version.hpp
-//	Date:	17 Jan 2020
+//	Date:	23 Jan 2020
 //
 //	Copyright (c) 2017 - 2020, Igor Baklykov
 //	All rights reserved.
@@ -19,11 +19,22 @@
 #include <arch/types.hpp>
 
 
-constexpr static dword_t	IGROS_VERSION_MAJOR	= 0u;
-constexpr static dword_t	IGROS_VERSION_MINOR	= 1u;
-constexpr static dword_t        IGROS_VERSION_BUILD	= 0u;
+constexpr static byte_t IGROS_VERSION_MAJOR	= 0u;
+constexpr static byte_t IGROS_VERSION_MINOR	= 1u;
+constexpr static word_t IGROS_VERSION_BUILD	= 0u;
 
-constexpr static const sbyte_t*	IGROS_VERSION_NAME	= "ALPHA";
+constexpr static const sbyte_t* const IGROS_VERSION_NAME	= u8"ALPHA";
+
+
+// Kernel version integer
+constexpr dword_t IGROS_KERNEL_VERSION(const byte_t major, const byte_t minor, const dword_t build) noexcept {
+	return (build | (static_cast<dword_t>(minor) << 16u) | (static_cast<dword_t>(major) << 24u));
+}
+
+// Kernel current version integer
+constexpr dword_t IGROS_KERNEL_VERSION_CURRENT() noexcept {
+	return IGROS_KERNEL_VERSION(IGROS_VERSION_MAJOR, IGROS_VERSION_MINOR, IGROS_VERSION_BUILD);
+}
 
 
 #endif	// IGROS_VERSION_HPP
