2458324f02fc17fe085df47f6703158c69a2f947
IGR2014 <igooor7@gmail.com>
2021-07-05 17:33:41 +0300

    (i386) FPU fix [2]

--- arch/i386/boot/fpu.s
+++ arch/i386/boot/fpu.s
@@ -11,7 +11,6 @@
 #
 
 
-.set	FPU_STATUS_WORD_TEST,	0xDEAD		# FPU status word test
 .set	FPU_CONTROL_WORD_MASK,	0x103F		# FPU control word mask
 .set	FPU_CONTROL_WORD,	0x003F		# FPU control word
 
@@ -27,17 +26,26 @@
 .type fpuCheck, @function
 fpuCheck:
 	fninit					# Initialize FPU
-	fnstsw $FPU_STATUS_WORD_TEST		# Store FPU status word
+	movl $FPU_STATUS_WORD_TEST, %ax		# Load status test value to AX
+	fnstsw 2(%ax)				# Store FPU status word
 	cmpw $0, %ax				# Compare FPU status word with 0
 	jne 1f					# Not equal - no FPU
-	fnstcw %ax				# Store FPU control word
+	fnstcw 2(%ax)				# Store FPU control word
 	andw $FPU_CONTROL_WORD_MASK, %ax	# Apply mask to FPU control word
 	cmpw $FPU_CONTROL_WORD, %ax		# Compare FPU control word with 0
 	jne 1f					# Not equal - no FPU
 	movl $1, %eax				# FPU - ok
 	retl					# Exit
 1:
-	movl $0, $eax				# FPU - error
+	movl $0, %eax				# FPU - error
 	retl					# Exit
 .size fpuCheck, . - fpuCheck
+
+
+.section .data
+.balign	4096
+
+# FPU status word test
+FPU_STATUS_WORD_TEST:
+	.word	0xDEAD
 
