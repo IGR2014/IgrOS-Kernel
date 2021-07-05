fd77a959b693447ba21a562b2529fbcb5939a4e5
IGR2014 <igooor7@gmail.com>
2021-07-05 16:56:07 +0300

    (cmake) Fixed jobs parallelism

--- config/script/deploy-all.sh
+++ config/script/deploy-all.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cmake -DIGROS_ARCH=i386 -DCMAKE_TOOLCHAIN_FILE=config/cmake/clang++-i386.cmake -DCMAKE_BUILD_TYPE=Release -S ./ -B ./build/i386
-cmake --build ./build/i386 --target install --jobs 6
+cmake --build ./build/i386 --target install --parallel
 
 cmake -DIGROS_ARCH=x86_64 -DCMAKE_TOOLCHAIN_FILE=config/cmake/clang++-x86_64.cmake -DCMAKE_BUILD_TYPE=Release -S ./ -B ./build/x86_64
-cmake --build ./build/x86_64 --target install --jobs 6
+cmake --build ./build/x86_64 --target install --parallel
 
--- config/script/deploy-i386.sh
+++ config/script/deploy-i386.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cmake -DIGROS_ARCH=i386 -DCMAKE_TOOLCHAIN_FILE=config/cmake/clang++-i386.cmake -DCMAKE_BUILD_TYPE=Release -S ./ -B ./build/i386
-cmake --build ./build/i386 --target install --jobs 6
+cmake --build ./build/i386 --target install --parallel
 
