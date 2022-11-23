47c8d47a1014b8c5ef93c1b4daaa6de5ad94ba81
IGR2014 <igooor7@gmail.com>
2022-11-23 23:30:27 +0200

    (CMake) CMake improvements + CCache

---
+++ cmake/modules/EnableCCache.cmake
@@ -0,0 +1,33 @@
+# - Find CCache
+# Find the CCache and enables it
+#
+
+
+# Look for CCache
+find_program(CCACHE ccache)
+
+
+# CCache found
+if(CCACHE)
+
+
+	# Enable CCache for C
+	set(CMAKE_C_COMPILER_LAUNCHER ${CCACHE})
+	# Enable CCache for C++
+	set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
+
+
+	# Message
+	message(STATUS "Found CCache: ${CCACHE}")
+
+
+# No CCache found
+else()
+
+
+	# Message
+	message(STATUS "Coud NOT find CCache: (missing CCACHE)")
+
+
+endif()
+
